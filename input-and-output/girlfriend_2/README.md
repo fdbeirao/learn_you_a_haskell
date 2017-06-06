8dd3013081eeb6192be080a8a95a9cdf9288b826
Fábio Beirão <fdbeirao@gmail.com>
2017-06-06 22:11:42 +0200

    Implementation of girlfriend (from Avril Lavigne)

---
+++ input-and-output/girlfriend_2/README.md
@@ -0,0 +1,11 @@
+## Short lines
+
+Taken from [Learn you a Haskell](http://learnyouahaskell.com/input-and-output#files-and-streams)
+
+## Compile
+
+> ghc --make ./girlfriend.hs
+
+## Execution
+
+> ./girlfriend
---
+++ input-and-output/girlfriend_2/girlfriend.hs
@@ -0,0 +1,17 @@
+import System.IO
+
+main :: IO ()
+main = do
+    withFile "girlfriend.txt" ReadMode (\handle -> do
+      contents <- hGetContents handle
+      putStr contents)
+
+
+-- Here's how we can make out own `withFile` function
+
+withFile' :: FilePath -> IOMode -> (Handle -> IO a) -> IO a
+withFile' path mode f = do
+    handle <- openFile path mode
+    result <- f handle
+    hClose handle
+    return result

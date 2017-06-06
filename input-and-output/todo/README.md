8c59959fdb9c10a47133978fb872e5af2786af98
Fábio Beirão <fdbeirao@gmail.com>
2017-06-06 22:55:35 +0200

    Add implementation of todo

---
+++ input-and-output/todo/README.md
@@ -0,0 +1,15 @@
+## Todo
+
+Taken from [Learn you a Haskell](http://learnyouahaskell.com/input-and-output#files-and-streams)
+
+## Execution without compilation
+
+```
+runhaskell ./todo
+```
+
+## Compilation && Execution
+
+```
+ghc --make ./todo.hs; ./todo
+```
---
+++ input-and-output/todo/todo.hs
@@ -0,0 +1,22 @@
+import System.IO (openFile, openTempFile, hGetContents, getLine, putStrLn, putStr, hPutStr, hClose, IOMode(ReadMode))
+import System.Directory (removeFile, renameFile)
+import Data.List (delete, (!!))
+
+main :: IO ()
+main = do
+    handle <- openFile "todo.txt" ReadMode
+    contents <- hGetContents handle
+    let todoTasks = lines contents
+        numberedTasks = zipWith (\n line -> show n ++ " - " ++ line) [0..] todoTasks
+    putStrLn "These are your TO-DO items:"
+    putStr $ unlines numberedTasks
+    putStrLn "Which one do you want to delete?"
+    numberString <- getLine
+    let number = read numberString
+        newTodoItems = delete (todoTasks !! number) todoTasks
+    (tempName, tempHandle) <- openTempFile "." "temp"
+    hPutStr tempHandle $ unlines newTodoItems
+    hClose handle
+    hClose tempHandle
+    removeFile "todo.txt"
+    renameFile tempName "todo.txt"
